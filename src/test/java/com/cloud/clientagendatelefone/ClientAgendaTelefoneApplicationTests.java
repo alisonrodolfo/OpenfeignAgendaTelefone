716d2f231529294a2981a3a9a0e65cc8da1a6886
Alison Barreiro <alison.rmb@gmail.com>
2022-01-27 00:52:07 -0300

    Class test

---
+++ src/test/java/com/cloud/clientagendatelefone/ClientAgendaTelefoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.clientagendatelefone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAgendaTelefoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
